5128af63a4e7476932f4aa3c3c29ac13818d39bb
unknown <trieunguyen30@gmail.com>
2017-12-13 21:09:25 +0700

    update

--- Broadcast/Program.cs
+++ Broadcast/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
+using System.Net.Sockets;
 namespace Broadcast
 {
     class Program
     {
         static void Main(string[] args)
         {
+            {
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 9050);
+                IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("192.168.1.255"), 9050);
+                string hostname = Dns.GetHostName();
+                byte[] data = Encoding.ASCII.GetBytes(hostname);
+                sock.SetSocketOption(SocketOptionLevel.Socket,
+                SocketOptionName.Broadcast, 1);
+                sock.SendTo(data, iep1);
+                sock.SendTo(data, iep2);
+                sock.Close();
+            }
         }
     }
 }
